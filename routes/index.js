15a369fc8261cbbe2b0e63a0f850050230b0fa0b
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2022-09-28 21:01:56 -0400

    test link pdf

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,61 @@
 var express = require("express");
 var router = express.Router();
 
+const currentYear = new Date ().getFullYear();
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { 
+    title: "Home",
+    year: currentYear
+   });
 });
 
 /* GET home page. */
 router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { 
+    title: "Home",
+    year: currentYear 
+  });
 });
 
 /* GET about me page. */
 router.get("/about", function (req, res, next) {
-  res.render("index", { title: "About Me" });
+  res.render("index", { 
+    title: "About Me",
+    year: currentYear 
+  });
 });
 
 /* GET projects page. */
 router.get("/projects", function (req, res, next) {
-  res.render("index", { title: "Projects" });
+  res.render("index", { 
+    title: "Projects",
+    year: currentYear 
+  });
 });
 
 /* GET services page. */
 router.get("/services", function (req, res, next) {
-  res.render("index", { title: "Services" });
+  res.render("index", { 
+    title: "Services",
+    year: currentYear 
+  });
 });
 
 /* GET contact page. */
 router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "Contact" });
+  res.render("index", { 
+    title: "Contact",
+    year: currentYear 
+  });
 });
 
 router.get("/resume", function (req, res, next) {
-  res.render("index", { title: "Resume" });
+  res.render("index", { 
+    title: "Resume",
+    year: currentYear 
+  });
 });
 
 module.exports = router;
